dcf0ceaff168675bfb770eb2f8121b64c9140e1d
kenny <kenny.alvarez@udea.edu.co>
2023-02-22 09:14:35 -0500

    sonidos js para las cuerdas

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-
-
-
+// teclado//
 
 var context= new AudioContext();
 
@@ -14,6 +12,50 @@ g.connect(context.destination);
 o.start(0);
 g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1,5);
 }
+
+
+// instrumentos//
+
+let numDeBotones = document.querySelectorAll(".img").length;
+
+for (let i = 0; i<numDeBotones; i++) {
+
+  document.querySelectorAll("button")[i].addEventListener("click", function() {
+   
+    var buttonInnerHTML = this.innerHTML;
+
+    switch(buttonInnerHTML) {
+      case "a":
+      var bajo = new Audio("sonidos/contrabajo.mp3");
+      bajo.play();
+      break;
+
+      case "b":
+      var violin = new Audio("sonidos/A3.mp3");
+      violin.play();
+      break;
+
+      case "c":
+      var cello = new Audio("sonidos/C3.mp3");
+      cello.play();
+      break;
+
+      case "d":
+      var viola = new Audio("sonidos/B3.mp3");
+      viola.play();
+      break;
+
+      default: console.log(buttonInnerHTML);
+    }
+
+
+
+  });
+
+}
+
+
+
 
 
 
